d91fdc4a2c42220406cbffdde7f334d51f3df24e
toohotz <gailsx@gmail.com>
2015-01-30 09:20:19 -0500

    Made screen appearance of text more UI friendly

--- SWAGDemo/DetailViewController.m
+++ SWAGDemo/DetailViewController.m
@@ -22,6 +22,14 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *checkedOutTextField;
 
+// Static properties
+
+@property (weak, nonatomic) IBOutlet UILabel *publisherLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *tagsLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *checkoutButton;
+
 @end
 
 @implementation DetailViewController
@@ -46,6 +54,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
+        _authorTextField.alpha = 0;
+        _titleTextField.alpha = 0;
+        _publisherTextField.alpha = 0;
+        _tagsTextField.alpha = 0;
+        _publisherLabel.alpha = 0;
+        _tagsLabel.alpha = 0;
+        _checkedOutTextField.alpha = 0;
+        _checkoutButton.alpha = 0;
+    
+    
     
     /// populate the labels and textview with book information
     void (^populateBookInformation)(void) =
@@ -59,11 +77,11 @@
     };
     
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5* NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0* NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         populateBookInformation();
+        [self fadeInText];
     });
     
-    NSLog(@"Last checked out by %@",[ServerBookManager lastCheckedOutBy]);
     
 }
 
@@ -71,5 +89,32 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Fade In Text Transition
+/**
+ @description Fades in text onto screen
+ */
+-(void)fadeInText
+{
+    /*
+     This function's purpose is to give the View Controller time to 
+     access the ServerBookManager and retrieve the neccessary information
+     in a user friendly manner
+     */
+    
+    [UIView animateWithDuration:0.65f animations:^{
+        _authorTextField.alpha = 1;
+        _titleTextField.alpha = 1;
+        _publisherTextField.alpha = 1;
+        _tagsTextField.alpha = 1;
+        _publisherLabel.alpha = 1;
+        _tagsLabel.alpha = 1;
+        _checkedOutTextField.alpha = 1;
+        _checkoutButton.alpha = 1;
+
+    }];
+    
+}
+
 
 @end
