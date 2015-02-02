bc767df4b75d82d0bff330fddd4eecf6ade0c69d
toohotz <gailsx@gmail.com>
2015-02-02 09:30:46 -0500

    Fixed an issue where an "empty" book could be created on the server

--- SWAGDemo/AddBookViewController.m
+++ SWAGDemo/AddBookViewController.m
@@ -8,7 +8,6 @@
 
 #import "AddBookViewController.h"
 #import "ServerBookManager.h"
-#import "BookValues.h"
 
 @interface AddBookViewController ()
 
@@ -48,31 +47,44 @@
     
     __block NSDictionary *paramters;
     
-    void (^singletonSetters)(void) =
-    ^{
-        paramters = @{@"author": _authorTF.text,
-                      @"title": _bookTitleTF.text,
-                      @"publisher": _publisherTF.text,
-                      @"categories": _categoriesTF.text,
-                      @"lastCheckedOut": @"",
-                      @"lastCheckedOutBy": @"",
-                      @"id": [ServerBookManager nextBookNumber],
-                      };
-    };
     
-    singletonSetters();
+//    make sure all fields are valid so "empty" books aren't created on server
     
-    NSLog(@"The author from add book is %@", [[BookValues sharedManager] author]);
-    
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [ServerBookManager createNewBookWithInformation:paramters];
+    if ((_authorTF.text.length >= 1 && _authorTF.text.length <= 3) || (_bookTitleTF.text.length >= 1 && _bookTitleTF.text.length <= 3) || (_publisherTF.text.length >= 1 && _publisherTF.text.length <= 3) || (_categoriesTF.text.length >= 1 && _categoriesTF.text.length <= 3) || _authorTF.text.length == 0  || _bookTitleTF.text.length == 0 || _publisherTF.text.length == 0 || _categoriesTF.text.length == 0) {
         
-    });
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Oops!" message:@"It seems that you have left a field blank" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+        [alert show];
+    }
     
-    [self.navigationController popToRootViewControllerAnimated:YES];
-    
-    
+        else {
+        [self.navigationController popToRootViewControllerAnimated:YES];
+        
+        
+        
+        void (^singletonSetters)(void) =
+        ^{
+            paramters = @{@"author": _authorTF.text,
+                          @"title": _bookTitleTF.text,
+                          @"publisher": _publisherTF.text,
+                          @"categories": _categoriesTF.text,
+                          @"lastCheckedOut": @"",
+                          @"lastCheckedOutBy": @"",
+                          @"id": [ServerBookManager nextBookNumber],
+                          };
+        };
+        
+        singletonSetters();
+        
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [ServerBookManager createNewBookWithInformation:paramters];
+            
+        });
+        
+        [self.navigationController popToRootViewControllerAnimated:YES];
+        
+    }
+  
 }
 
 
@@ -108,40 +120,7 @@
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [textField resignFirstResponder];
-    
-
-    /// Verifies that user has filled in all fields
-    BOOL (^userFieldsCompleted)(void) =
-    ^{
-        BOOL finished;
-        
-        if (textField.text.length < 1) {
-            finished = FALSE;
-        }
-        
-        else
-        {
-            finished = TRUE;
-        }
-        
-        return finished;
-    };
-    
-    
-    
-    if (userFieldsCompleted() == TRUE) {
-        NSLog(@"All fields filled in.");
- 
-        [self.navigationController popViewControllerAnimated:YES];
-        
-        
-    }
-    else if (userFieldsCompleted() == FALSE)
-    {
-        NSLog(@"Some fields are missing.");
-    }
-    
-
+   
     
     
     return TRUE;
